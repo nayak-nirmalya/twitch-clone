5f06eb4ea116baaee01d40bdc817f03a983997f2
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-14 14:05:08 +0530

    added search component in navbaer

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,7 +1,60 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import qs from "query-string";
+import { SearchIcon, X } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export function Search() {
-  return <div>Search</div>;
+  const router = useRouter();
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!value) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/search",
+        query: { term: value },
+      },
+      { skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
+
+  const onClear = () => setValue("");
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="relative w-full lg:w-[400px] flex items-center"
+    >
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Search"
+        className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+      />
+      {value && (
+        <X
+          onClick={onClear}
+          className="absolute top-2.5 right-14 h-5 w-5 text-muted-foreground cursor-pointer hover:opacity-75 transition"
+        />
+      )}
+      <Button
+        className="rounded-l-none"
+        type="submit"
+        size="sm"
+        variant="secondary"
+      >
+        <SearchIcon className="h-5 w-5 text-muted-foreground" />
+      </Button>
+    </form>
+  );
 }
