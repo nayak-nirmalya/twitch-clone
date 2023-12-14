73d8a23d520d7accf58e0826fcc82bedb85cf7cf
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-14 13:42:33 +0530

    added logo to navbar

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import { Logo } from "./logo";
+
 export function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
+      <Logo />
+    </div>
+  );
 }
---
+++ app/(browse)/_components/navbar/logo.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1">
+          <Image src="/spooky.svg" alt="GameHub" height="32" width="32" />
+        </div>
+        <div className={cn(font.className)}>
+          <p className="text-lg font-semibold">GameHub</p>
+          <p className="text-xs text-muted-foreground">Let&apos;s Play</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
