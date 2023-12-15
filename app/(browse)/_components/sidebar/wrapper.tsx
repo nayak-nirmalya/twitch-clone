d2cee70e941a286cdc9f39f0b015a5f7f18490c4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-15 18:44:58 +0530

    added npm pkg: zustand

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function Wrapper({ children }: { children: React.ReactNode }) {
-  return <aside>{children}</aside>;
+  return (
+    <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+      {children}
+    </aside>
+  );
 }
