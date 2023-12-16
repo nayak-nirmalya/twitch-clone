e3c5ad2922886422c76a17ef360b3a3424e8e318
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-16 17:57:05 +0530

    added new lib function: getSelf & getRecommended

---
+++ lib/auth-service.ts
@@ -0,0 +1,23 @@
+import { currentUser } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+
+export const getSelf = async () => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      externalUserId: self.id,
+    },
+  });
+
+  if (!user) {
+    throw new Error("Not found");
+  }
+
+  return user;
+};
