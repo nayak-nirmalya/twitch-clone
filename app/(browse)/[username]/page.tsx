a10c4d1885519f1a755ce09d87e9f9c6d32c42c4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-19 09:31:25 +0530

    added helper fn: getUserById/Username

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
-export default function UserPage({
+import { getUserByUsername } from "@/lib/user-service";
+
+export default async function UserPage({
   params: { username },
 }: {
   params: { username: string };
 }) {
-  return <div>User: {username}</div>;
+  const user = await getUserByUsername(username);
+
+  if (!user) notFound();
+
+  return <div className="flex flex-col gap-y-4">User: {user?.username}</div>;
 }
---
+++ lib/user-service.ts
@@ -0,0 +1,21 @@
+import { db } from "@/lib/db";
+
+export const getUserByUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  return user;
+};
+
+export const getUserById = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return user;
+};
