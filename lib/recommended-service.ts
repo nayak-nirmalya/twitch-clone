7131fa7dceb47fd3fbff2ba6641bd23dbf915f4a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-19 08:56:52 +0530

    filter current logged in user from rec list

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,13 +2,35 @@ import { db } from "@/lib/db";
 import { getSelf } from "@/lib/auth-service";
 
 export const getRecommended = async () => {
-  await new Promise((resove) => setTimeout(resove, 6000));
+  let userId;
 
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch (error) {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
   return users;
 };
