fa5871b438eb8a570d9f112c4affc794194b3fd1
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-20 19:42:21 +0530

    removed blocked user form following and blocked list

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -8,6 +8,13 @@ export const getFollwedUser = async () => {
     const followedUsers = await db.follow.findMany({
       where: {
         followerId: self.id,
+        following: {
+          blocking: {
+            none: {
+              blockedId: self.id,
+            },
+          },
+        },
       },
       include: {
         following: true,
