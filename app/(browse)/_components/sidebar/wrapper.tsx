55c8e57075c02822f558a97109ee6acd906acc9e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-20 18:27:17 +0530

    minor bux fix: sidebar width on collapse

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -26,7 +26,7 @@ export function Wrapper({ children }: { children: React.ReactNode }) {
   return (
     <aside
       className={cn(
-        "fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
         collapsed && "w-[70px]"
       )}
     >
