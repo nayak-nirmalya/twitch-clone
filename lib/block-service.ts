c9ae14cb984c49e3372bafaebc121a4dad1b01f4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-20 19:04:27 +0530

    added isBlockedByUser fn to check wheather user bocked

---
+++ lib/block-service.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+export const isBlockedByUser = async (id: string) => {
+  try {
+    const self = await getSelf();
+
+    const otherUser = await db.user.findUnique({
+      where: { id },
+    });
+
+    if (!otherUser) throw new Error("User not found");
+
+    if (otherUser.id === self.id) return false;
+
+    const existingBlock = await db.block.findUnique({
+      where: {
+        blockerId_blockedId: {
+          blockerId: otherUser.id,
+          blockedId: self.id,
+        },
+      },
+    });
+
+    return !!existingBlock;
+  } catch {
+    return false;
+  }
+};
