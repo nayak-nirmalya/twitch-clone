0cd4ea482a6b8447bd0822819302fdd91ac94cc3
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-20 19:11:22 +0530

    added helper fn un/blockUser

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -27,3 +27,72 @@ export const isBlockedByUser = async (id: string) => {
     return false;
   }
 };
+
+export const blockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) throw new Error("Cannot block yourself");
+
+  const otherUser = await db.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) throw new Error("User not found");
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (existingBlock) throw new Error("User is already blocked");
+
+  const block = await db.block.create({
+    data: {
+      blockerId: self.id,
+      blockedId: otherUser.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return block;
+};
+
+export const unblockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) throw new Error("Cannot unblock yourself");
+
+  const otherUser = await db.user.findUnique({
+    where: { id },
+  });
+
+  if (!otherUser) throw new Error("User not found");
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (!existingBlock) throw new Error("User is not blocked");
+
+  const unblock = await db.block.delete({
+    where: {
+      id: existingBlock.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return unblock;
+};
