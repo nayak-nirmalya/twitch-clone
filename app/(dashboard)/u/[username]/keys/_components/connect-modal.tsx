8b0beda2152611b62188ed7cef33de66fd4c8e26
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-24 14:16:08 +0530

    added connection type selection modal

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -11,6 +11,13 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { AlertTriangle } from "lucide-react";
 
 export function ConnectModal() {
@@ -23,6 +30,15 @@ export function ConnectModal() {
         <DialogHeader>
           <DialogTitle>Generate connection</DialogTitle>
         </DialogHeader>
+        <Select>
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Ingress Type" />
+            <SelectContent>
+              <SelectItem value="RTMP">RTMP</SelectItem>
+              <SelectItem value="WHIP">WHIP</SelectItem>
+            </SelectContent>
+          </SelectTrigger>
+        </Select>
         <Alert>
           <AlertTriangle className="h-4 w-4" />
           <AlertTitle>Warning!</AlertTitle>
@@ -31,6 +47,14 @@ export function ConnectModal() {
             connection.
           </AlertDescription>
         </Alert>
+        <div className="flex justify-between">
+          <DialogClose>
+            <Button variant="ghost">Cancel</Button>
+          </DialogClose>
+          <Button onClick={() => {}} variant="primary">
+            Generate
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
