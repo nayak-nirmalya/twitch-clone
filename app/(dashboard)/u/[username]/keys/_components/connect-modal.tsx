eaa2d26c250cb93130251ac2ca9dcaaaefbd2ea9
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-24 14:01:36 +0530

    added warnign alert to modal

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -10,6 +10,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
 
 export function ConnectModal() {
   return (
@@ -17,6 +19,19 @@ export function ConnectModal() {
       <DialogTrigger asChild>
         <Button variant="primary">Generate connection</Button>
       </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Generate connection</DialogTitle>
+        </DialogHeader>
+        <Alert>
+          <AlertTriangle className="h-4 w-4" />
+          <AlertTitle>Warning!</AlertTitle>
+          <AlertDescription>
+            This action will reset all active streams using the currnet
+            connection.
+          </AlertDescription>
+        </Alert>
+      </DialogContent>
     </Dialog>
   );
 }
