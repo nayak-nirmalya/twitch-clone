96b56cee7a08e950020dcd8e96417cb88cddde30
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-24 10:52:19 +0530

    added CopyButton to copy ingest server url

--- app/(dashboard)/u/[username]/keys/_components/copy_button.tsx
+++ app/(dashboard)/u/[username]/keys/_components/copy_button.tsx
@@ -1,7 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { CheckIcon, Copy } from "lucide-react";
 
-export function CopyButton({ value }: { value: string | null }) {
-  return <div>CopyButton</div>;
+import { Button } from "@/components/ui/button";
+
+export function CopyButton({ value }: { value?: string }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const onCopy = () => {
+    if (!value) return;
+
+    setIsCopied(true);
+    navigator.clipboard.writeText(value);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  };
+
+  const Icon = isCopied ? CheckIcon : Copy;
+
+  return (
+    <Button
+      onClick={onCopy}
+      disabled={!value || isCopied}
+      variant="ghost"
+      size="sm"
+    >
+      <Icon className="h-4 w-4" />
+    </Button>
+  );
 }
