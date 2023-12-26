432a65cb4a3e99932848abb2e3b5de40ede2057e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-26 14:48:11 +0530

    generate user id to watch stream

--- components/stream-player.tsx
+++ components/stream-player.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { Stream, User } from "@prisma/client";
 
+import { useViewerToken } from "@/hooks/use-viewer-token";
+
 export default function StreamPlayer({
   user,
   stream,
@@ -12,5 +14,11 @@ export default function StreamPlayer({
   stream: Stream;
   isFollwing: boolean;
 }) {
-  return <div>StreamPlayer</div>;
+  const { identity, name, token } = useViewerToken(user.id);
+
+  if (!token || !identity || !name) {
+    return <div>Can not watch the stream.</div>;
+  }
+
+  return <div>Allowed to watch stream.</div>;
 }
--- hooks/use-viewer-token.ts
+++ hooks/use-viewer-token.ts
@@ -2,6 +2,8 @@ import { toast } from "sonner";
 import { useEffect, useState } from "react";
 import { JwtPayload, jwtDecode } from "jwt-decode";
 
+import { createViewerToken } from "@/actions/token";
+
 export const useViewerToken = (hostIdentity: string) => {
   const [token, setToken] = useState("");
   const [name, setName] = useState("");
@@ -10,10 +12,34 @@ export const useViewerToken = (hostIdentity: string) => {
   useEffect(() => {
     const createToken = async () => {
       try {
-        const viewerToken = await fetch;
+        const viewerToken = await createViewerToken(hostIdentity);
+        setToken(viewerToken);
+
+        const decodedToken = jwtDecode(viewerToken) as JwtPayload & {
+          name?: string;
+        };
+
+        const name = decodedToken?.name;
+        const identity = decodedToken.jti;
+
+        if (identity) {
+          setIdentity(identity);
+        }
+
+        if (name) {
+          setName(name);
+        }
       } catch (error) {
         toast.error("Something went wrong! Error creating token");
       }
     };
-  }, []);
+
+    createToken();
+  }, [hostIdentity]);
+
+  return {
+    token,
+    name,
+    identity,
+  };
 };
