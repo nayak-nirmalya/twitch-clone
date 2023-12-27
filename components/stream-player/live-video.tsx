dc64ac5c2b944dd16c7ad06df96b3c1ec006fa7c
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-27 19:23:21 +0530

    show live stream

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -35,6 +35,14 @@ export function LiveVideo({ participant }: { participant: Participant }) {
     onVolumeChange(0);
   }, []);
 
+  useTracks([Track.Source.Camera, Track.Source.Microphone])
+    .filter((track) => track.participant.identity === participant.identity)
+    .forEach((track) => {
+      if (videoRef.current) {
+        track.publication.track?.attach(videoRef.current);
+      }
+    });
+
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videoRef} width="100%" />
