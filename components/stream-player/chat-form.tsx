663ad279b72eebd903c7351c45257a4b3bcc2c41
Nirmalya Nayak <n.nayak263@gmail.com>
2023-12-29 17:21:49 +0530

    added chat input & chat form skeleton

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -24,5 +24,67 @@ export function ChatForm({
   isDelayed: boolean;
   isFollowing: boolean;
 }) {
-  return <div>ChatForm</div>;
+  const [isDelayBlocked, setIsDelayBlocked] = useState(false);
+
+  const isFollowersOnlyAndNotFollowing = isFollowersOnly && !isFollowing;
+  const isDisabled =
+    isHidden || isDelayBlocked || isFollowersOnlyAndNotFollowing;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!value || isDisabled) return;
+
+    if (isDelayed && !isDelayBlocked) {
+      setIsDelayBlocked(true);
+      setTimeout(() => {
+        setIsDelayBlocked(false);
+        onSubmit();
+      }, 3000);
+    } else {
+      onSubmit();
+    }
+  };
+
+  if (isHidden) {
+    return null;
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center gap-y-4 p-3"
+    >
+      <div className="w-full">
+        <Input
+          onChange={(e) => onChange(e.target.value)}
+          value={value}
+          disabled={isDisabled}
+          placeholder="Send a message"
+          className={cn(
+            "border-white/10",
+            isFollowersOnly && "rounded-t-none border-t-0"
+          )}
+        />
+      </div>
+      <div className="ml-auto">
+        <Button type="submit" variant="primary" size="sm" disabled={isDisabled}>
+          Chat
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export function ChatFormSkeleton() {
+  return (
+    <div className="flex flex-col items-center gap-y-4 p-3">
+      <Skeleton className="w-full h-10" />
+      <div className="flex items-center gap-x-2 ml-auto">
+        <Skeleton className="h-7 w-7" />
+        <Skeleton className="h-7 w-12" />
+      </div>
+    </div>
+  );
 }
