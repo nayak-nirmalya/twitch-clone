da7d5033b8c16dc3f1cbe7479b7f247d1cd47692
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-01 11:02:55 +0530

    dynamicaly show viewcount/status in header compnent

--- components/stream-player/header.tsx
+++ components/stream-player/header.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import React from "react";
+import {
+  useParticipants,
+  useRemoteParticipant,
+} from "@livekit/components-react";
+import { UserIcon } from "lucide-react";
 
 import { UserAvatar } from "@/components/user-avatar";
 import { VerifiedMark } from "@/components/verified-mark";
@@ -20,6 +25,15 @@ export function Header({
   isFollowing: boolean;
   name: string;
 }) {
+  const participants = useParticipants();
+  const participant = useRemoteParticipant(hostIdentity);
+
+  const isLive = !!participant;
+  const participantCount = participants.length - 1;
+
+  const hostAsViewer = `host-${hostIdentity}`;
+  const isHost = hostAsViewer === viewerIdentity;
+
   return (
     <div className="flex flex-col lg:flex-row gap-y-4 lg:gap-y-0 items-start justify-between px-4">
       <div className="flex items-center gap-x-3">
@@ -27,7 +41,7 @@ export function Header({
           imageUrl={imageUrl}
           username={hostName}
           size="lg"
-          isLive={true}
+          isLive={isLive}
           showBadge
         />
         <div className="space-y-1">
@@ -35,6 +49,20 @@ export function Header({
             <h2 className="text-lg font-semibold">{hostName}</h2>
             <VerifiedMark />
           </div>
+          <p className="text-sm font-semibold">{name}</p>
+          {isLive ? (
+            <div className="font-semibold flex gap-x-1 items-center text-xs text-rose-500">
+              <UserIcon className="h-4 w-4" />
+              <p>
+                {participantCount}{" "}
+                {participantCount === 1 ? "viewer" : "viewers"}
+              </p>
+            </div>
+          ) : (
+            <p className="font-semibold text-xs text-muted-foreground">
+              Offline
+            </p>
+          )}
         </div>
       </div>
     </div>
