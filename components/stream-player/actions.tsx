84770c1ddb67f928c4c52050b6e8220560cd4eca
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-01 15:30:06 +0530

    added un/follow fn to actions

--- components/stream-player/actions.tsx
+++ components/stream-player/actions.tsx
@@ -1,10 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { useTransition } from "react";
 import { Heart } from "lucide-react";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
+import { onFollow, onUnfollow } from "@/actions/follow";
 
 export function Actions({
   hostIdentity,
@@ -15,10 +20,49 @@ export function Actions({
   isFollowing: boolean;
   isHost: boolean;
 }) {
+  const { userId } = useAuth();
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleFollow = () => {
+    startTransition(() => {
+      onFollow(hostIdentity)
+        .then((data) =>
+          toast.success(`You are now following ${data.following.username}.`)
+        )
+        .catch(() => toast.error("Something went wrong while following."));
+    });
+  };
+
+  const handleUnfollow = () => {
+    startTransition(() => {
+      onUnfollow(hostIdentity)
+        .then((data) =>
+          toast.success(`You have unfollowed ${data.following.username}.`)
+        )
+        .catch(() => toast.error("Something went wrong while unfollowing."));
+    });
+  };
+
+  const toggleFollow = () => {
+    if (!userId) {
+      return router.push("/sign-in");
+    }
+
+    if (isHost) return;
+
+    if (isFollowing) {
+      handleUnfollow();
+    } else {
+      handleFollow();
+    }
+  };
+
   return (
     <Button
-      disabled={isHost}
-      onClick={() => {}}
+      disabled={isPending || isHost}
+      onClick={toggleFollow}
       variant="primary"
       size="sm"
       className="w-full lg:w-auto"
@@ -29,4 +73,8 @@ export function Actions({
       {isFollowing ? "Unfollow" : "Follow"}
     </Button>
   );
+}
+
+export function ActionsSkeleton() {
+  return <Skeleton className="h-10 w-full lg:w-24" />;
 }
