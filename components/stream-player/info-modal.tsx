0974b014a107e545f3756e01cf144841c8eb6fc2
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-02 19:04:45 +0530

    added state & styling to info-modal

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -27,5 +27,79 @@ export function InfoModal({
   initialName: string;
   initialThumbnailUrl: string | null;
 }) {
-  return null;
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const [name, setName] = useState(initialName);
+  const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link" size="sm" className="ml-auto">
+          Edit
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit stream info</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={() => {}} className="space-y-14">
+          <div className="space-y-2">
+            <Label>Name</Label>
+            <Input
+              disabled={isPending}
+              placeholder="Stream name"
+              onChange={onChange}
+              value={name}
+            />
+          </div>
+          <div className="space-y-2">
+            <Label>Thumbnail</Label>
+            {thumbnailUrl ? (
+              <div className="relative aspect-video rounded-xl overflow-hidden border border-white/10">
+                <div className="absolute top-2 right-2 z-[10]">
+                  <Hint label="Remove thumbnail" asChild side="left">
+                    <Button
+                      type="button"
+                      disabled={isPending}
+                      onClick={() => {}}
+                      className="h-auto w-auto p-1.5"
+                    >
+                      <Trash className="h-4 w-4" />
+                    </Button>
+                  </Hint>
+                </div>
+                <Image
+                  alt="Thumbnail"
+                  src={thumbnailUrl}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+            ) : (
+              <div className="rounded-xl border outline-dashed outline-muted">
+                {/* UploadDropzone */}
+              </div>
+            )}
+          </div>
+          <div className="flex justify-between">
+            <DialogClose ref={closeRef} asChild>
+              <Button type="button" variant="ghost">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={isPending} variant="primary" type="submit">
+              Save
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
 }
