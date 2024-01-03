45737a75d310ebc0e1c15d1404c513bcd17e57a8
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-03 13:38:26 +0530

    setup uploadthing for app router with nextjs

---
+++ app/api/api/uploadthing/core.ts
@@ -0,0 +1,34 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+
+const uploadthing = createUploadthing();
+
+export const ourFileRouter = {
+  thumbnailUploader: uploadthing({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(async () => {
+      const self = await getSelf();
+
+      return { user: self };
+    })
+    .onUploadComplete(async ({ file, metadata }) => {
+      await db.stream.update({
+        where: {
+          id: metadata.user.id,
+        },
+        data: {
+          thumbnailUrl: file.url,
+        },
+      });
+
+      return { fileUrl: file.url };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
