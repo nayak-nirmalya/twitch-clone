655d262f305ec55efdeda6d7a5441a475efde0e0
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-05 10:10:00 +0530

    addedf server action to updateUser

---
+++ actions/user.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { User } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+import { getSelf } from "@/lib/auth-service";
+import { db } from "@/lib/db";
+
+export const updateUser = async (values: Partial<User>) => {
+  try {
+    const self = await getSelf();
+
+    const validData = {
+      bio: values.bio,
+    };
+
+    const user = await db.user.update({
+      where: { id: self.id },
+      data: validData,
+    });
+
+    revalidatePath(`/${self.username}`);
+    revalidatePath(`/u/${self.username}`);
+
+    return user;
+  } catch {
+    throw new Error("Failed to update user");
+  }
+};
