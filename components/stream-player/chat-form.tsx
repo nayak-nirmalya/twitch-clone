65b13ed560f480f9dfb3674d0403a1d8f11fa177
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-06 10:23:59 +0530

    finor bug fix: border style in delayed mode

--- components/stream-player/chat-form.tsx
+++ components/stream-player/chat-form.tsx
@@ -67,7 +67,7 @@ export function ChatForm({
           placeholder="Send a message"
           className={cn(
             "border-white/10",
-            isFollowersOnly && "rounded-t-none border-t-0"
+            (isFollowersOnly || isDelayed) && "rounded-t-none border-t-0"
           )}
         />
       </div>
