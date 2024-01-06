6948f5bde933cf22dd78d530e0372e3c8b143ee2
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-06 10:11:45 +0530

    host can now block guest user

--- actions/block.ts
+++ actions/block.ts
@@ -1,20 +1,35 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
 
 import { blockUser, unblockUser } from "@/lib/block-service";
+import { getSelf } from "@/lib/auth-service";
+
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY,
+  process.env.LIVEKIT_API_SECRET
+);
 
 export const onBlock = async (id: string) => {
-  // TODO: Adapt to disconnect from live-stream
-  // TODO: Allow ability to kick the guest
+  const self = await getSelf();
 
-  const blockedUser = await blockUser(id);
+  let blockedUser;
 
-  revalidatePath("/");
-
-  if (blockedUser) {
-    revalidatePath(`/${blockedUser.blocked.username}`);
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    // this means user is guest
   }
+
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    // this means user not in the room
+  }
+
+  revalidatePath(`/u/${self.username}/community`);
 
   return blockedUser;
 };
