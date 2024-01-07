8a28ee378d2eb395932c1fea3fb8e391084297f7
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-07 18:49:35 +0530

    added getStreams fn to feed-service

---
+++ lib/feed-service.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+export const getStreams = async () => {
+  let userId;
+
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  let streams = [];
+
+  if (userId) {
+    streams = await db.stream.findMany({
+      where: {
+        user: {
+          NOT: {
+            blocking: {
+              some: {
+                blockedId: userId,
+              },
+            },
+          },
+        },
+      },
+      include: { user: true },
+      orderBy: [{ isLive: "desc" }, { updatedAt: "desc" }],
+    });
+  } else {
+    streams = await db.stream.findMany({
+      include: { user: true },
+      orderBy: [{ isLive: "desc" }, { updatedAt: "desc" }],
+    });
+  }
+};
