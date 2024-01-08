6ac6b98e000ff44dbd61e4bc15673e4b2de45930
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-08 08:34:52 +0530

    show stream/user-name alongside thumbnail on result card

--- app/(browse)/(home)/result-card.tsx
+++ app/(browse)/(home)/result-card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Stream, User } from "@prisma/client";
 
 import { Thumbnail } from "@/components/thumbnail";
+import { UserAvatar } from "@/components/user-avatar";
 
 export function ResultCard({ data }: { data: Stream & { user: User } }) {
   return (
@@ -14,6 +15,19 @@ export function ResultCard({ data }: { data: Stream & { user: User } }) {
           isLive={data.isLive}
           username={data.user.username}
         />
+        <div className="flex gap-x-3">
+          <UserAvatar
+            username={data.user.username}
+            imageUrl={data.user.imageUrl}
+            isLive={data.isLive}
+          />
+          <div className="flex flex-col text-sm overflow-hidden">
+            <p className="truncate font-semibold hover:text-blue-500">
+              {data.name}
+            </p>
+            <p className="text-muted-foreground">{data.user.username}</p>
+          </div>
+        </div>
       </div>
     </Link>
   );
