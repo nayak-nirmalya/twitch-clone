9a6d5ee79845131f3cba0d7c8b7468a7bb4e8828
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-09 14:27:01 +0530

    added ResultCardSkeleton for loading state

--- app/(browse)/search/_components/result-card.tsx
+++ app/(browse)/search/_components/result-card.tsx
@@ -7,7 +7,9 @@ import { Thumbnail, ThumbnailSkeleton } from "@/components/thumbnail";
 import { Skeleton } from "@/components/ui/skeleton";
 import { VerifiedMark } from "@/components/verified-mark";
 
-export function ResultCard({}: {
+export function ResultCard({
+  data,
+}: {
   data: {
     id: string;
     name: string;
@@ -17,5 +19,47 @@ export function ResultCard({}: {
     user: User;
   };
 }) {
-  return <div>ResultCard</div>;
+  return (
+    <Link href={`/${data.user.username}`}>
+      <div className="w-full flex gap-x-4">
+        <div className="relative h-[9rem] w-[16rem]">
+          <Thumbnail
+            src={data.thumbnailUrl}
+            fallback={data.user.imageUrl}
+            isLive={data.isLive}
+            username={data.user.username}
+          />
+        </div>
+        <div className="space-y-1">
+          <div className="flex items-center gap-x-2">
+            <p className="font-bold text-lg cursor-pointer hover:text-blue-500">
+              {data.user.username}
+            </p>
+            <VerifiedMark />
+          </div>
+          <p className="text-sm text-muted-foreground">{data.name}</p>
+          <p className="text-sm text-muted-foreground">
+            {formatDistanceToNow(new Date(data.updatedAt), {
+              addSuffix: true,
+            })}
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export function ResultCardSkeleton() {
+  return (
+    <div className="w-full flex gap-x-4">
+      <div className="relative h-[9rem] w-[16rem]">
+        <ThumbnailSkeleton />
+      </div>
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-32" />
+        <Skeleton className="h-3 w-24" />
+        <Skeleton className="h-3 w-12" />
+      </div>
+    </div>
+  );
 }
