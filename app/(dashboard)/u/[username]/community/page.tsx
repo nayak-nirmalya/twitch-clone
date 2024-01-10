8d1e8377c023c40758ddb1a159be16becba64bdc
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-10 08:39:38 +0530

    added new page: CommunityPage

---
+++ app/(dashboard)/u/[username]/community/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { format } from "date-fns";
+
+import {} from "@/lib/block-service";
+
+export default function CommunityPage() {
+  return <div>CommunityPage</div>;
+}
