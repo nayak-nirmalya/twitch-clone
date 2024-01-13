06d7c40159041273c0a9ce66d3cf1e79f27e982e
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-13 19:22:47 +0530

    changed homepage title metadat

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,6 +1,11 @@
 import React, { Suspense } from "react";
+import { Metadata } from "next";
 
 import { Results, ResultsSkeleton } from "./_components/results";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
 
 export default function Home() {
   return (
