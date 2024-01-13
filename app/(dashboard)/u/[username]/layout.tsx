479fc6589520bdce18f92cc796de1d97428325e3
Nirmalya Nayak <n.nayak263@gmail.com>
2024-01-13 19:01:51 +0530

    change title metadata for: Dashboard

--- app/(dashboard)/u/[username]/layout.tsx
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
 
 import { getSelfByUsername } from "@/lib/auth-service";
 
 import { Navbar } from "./_components/navbar";
 import { Sidebar } from "./_components/sidebar";
 import { Container } from "./_components/container";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
 
 export default async function CreatorLayout({
   children,
